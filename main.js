253ad9e662240085e34bddfbd8096f1e1374c3a1
SKPR15298 <shevakurnia87@gmail.com>
2024-02-27 22:57:00 +0700

    update-5

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+  var audio = document.getElementById("background-audio");
   const openButton = document.getElementById("get-content");
   const buttonAudio = document.getElementById("trigger");
   const autoplayAudio = document.querySelector("audio[autoplay]");
@@ -7,6 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const volumeChangeDuration = 1000;
   const volumeChangeInterval = 40;
   let volumeStep = 0.02;
+
+  audio.muted = false;
 
   openButton.addEventListener("click", function () {
     buttonAudio.play();
